a1376e7b5d0a59a8032b4c9c7d99b8803634bba3
joshtin2505 <justincastro2505@gmail.com>
2024-06-11 00:16:24 -0500

    feat: Add fade-light-gradient and fade-dark-gradient background images

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,14 @@ export default {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        "fade-light-gradient":
+          "linear-gradient(to right, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 20%, rgba(255, 255, 255, 0) 80%, rgba(255, 255, 255, 1) 100%)",
+        "fade-dark-gradient":
+          "linear-gradient(to right, rgba(0, 0, 0, 1) 0%, rgba(255, 255, 255, 0) 20%, rgba(255, 255, 255, 0) 80%, rgba(0, 0, 0, 1) 100%)"
+      }
+    }
   },
   plugins: [require("flowbite/plugin")],
   darkMode: "media"
